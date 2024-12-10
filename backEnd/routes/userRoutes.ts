b76150d20c0fc5003937e599328bdd09f508e074
Samuel Wachira <156680231+wachira-samuel@users.noreply.github.com>
2024-12-10 15:04:48 +0300

    Update userRoutes.ts

--- backEnd/routes/userRoutes.ts
+++ backEnd/routes/userRoutes.ts
@@ -1,12 +1,10 @@
-import express, { Router } from "express"; // Import Express and Router types
-import { registerUser, loginUser } from "../controllers/userController"; // Import registerUser and loginUser functions
+import express, { Router } from "express"; 
+import { registerUser, loginUser } from "../controllers/userController"; 
 
-const router: Router = express.Router(); // Create a router instance with explicit type
+const router: Router = express.Router(); 
 
-// Route to handle user registration
 router.post("/register", registerUser);
 
-// Route to handle user login
 router.post("/login", loginUser);
 
-export default router; // Export the router
+export default router; 
